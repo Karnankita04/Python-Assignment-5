3bbc9b68767b8296b2a7c7a11e6ff4b644b83d0d
Ankita karn <91003709+Karnankita04@users.noreply.github.com>
2022-09-24 11:27:37 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,65 @@
+# 1. Write a python script to remove the last digit from a given number. (for example, if
+# user enters 2534 then your output should be 253)
+# code: 
+# num=int(input("Enter a number: "))
+# print(num//10)
+
+# 2. Write a python script to get the last digit from a given number. (for example, if user
+# enters 2089 then your output should be 9)
+# code: 
+# num=int(input("Enter a number: "))
+# print(num%10)
+
+# 3. Write a python script to swap data of two variables
+# code: 
+# a=int(input('Enter value of a'))
+# b=int(input('Enter value of b')) 
+# # Python_style 
+# # a,b=b,a
+# # a=a*b 
+# # b=a//b
+# # a=a//b 
+# print("a =",a,"b =",b)
+
+# 4. Write a python script to find x power y, where values of x and y are given by user
+# code: 
+# x=int(input("Enter the base: "))
+# y=int(input("Enter the power: "))
+# print(x**y)
+
+# 5. Write a python script which takes a three digit number from the user and displays
+# only its first digit.
+# code: 
+# x=int(input("Enter a three digit number: "))
+# print((x//100))
+
+# 6. Write a python script which takes a three digit number from the user and displays
+# only its middle digit.
+# code: 
+# x=int(input("Enter a three digit number: "))
+# print((x//10)%10)
+
+# 7. Write a python script which takes a three digit number from the user and displays
+# only its last digit.
+# code: 
+# x=int(input("Enter a three digit number: "))
+# print(x%10)
+
+# 8. Write a python script to use IN operator to display the data present in the list
+# code: 
+# list=[1.3,'Ankita',5,True]
+# for i in list:
+#     print(i,end=",")
+
+# 9. Write a python script to use NOT IN operator to display the data not present in list
+# code: 
+# list=[1.3,'Ankita',5,True]
+# print(8 not in list)
+
+# 10. Write a python script to use IS operator to display if both variables are the same
+# object or not?
+# code: 
+# a=5
+# b=5
+# print(a is b) # "is" gives true if the id of both variable is same 
+
